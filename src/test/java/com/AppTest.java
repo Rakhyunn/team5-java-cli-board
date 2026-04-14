2c4cd2e9a1605a99d3533b688dbe97f716dc8a72
Rakhyunn <krh0323@pukyong.ac.kr>
2026-04-14 17:16:18 +0900

    [test] 슬라임 전투 테스트 코드 추가

--- src/test/java/com/AppTest.java
+++ src/test/java/com/AppTest.java
@@ -3,6 +3,8 @@ package com;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AppTest {
@@ -16,5 +18,31 @@ public class AppTest {
                 .contains("닉네임을 입력하여 주세요!")
                 .contains("닉네임 : ")
                 .contains("용사");
+    }
+
+    @Test
+    @DisplayName("슬라임과 전투 : 슬라임 처치")
+    void t2() {
+        String rs = AppTestRunner.run("""
+                용사
+                1
+                """);
+        assertThat(rs)
+                .contains("야생에서 슬라임 (이)가 나타났다!")
+                .contains("어떻게 하시겠습니까?(숫자 입력)")
+                .contains("1.전투 2.도망")
+                .contains("용사(이)가 슬라임(을)를 공격")
+                .contains("용사(이)가 슬라임(을)를 처치 성공");
+    }
+
+    @Test
+    @DisplayName("슬라임과 전투 : 전투 중 유저 사망")
+    void t3() {
+        Warrior warrior = new Warrior("용사");
+        warrior.setHp(1); // HP 1로 설정
+        Slime slime = new Slime();
+        BattleManager bm = new BattleManager(warrior, slime, new Scanner("1\n"));
+        bm.start();
+        assertThat(warrior.getDied()).isTrue();
     }
 }
